4704a635a73ff1049dd128ebcbe855958c475a3b
izaakm <1505395+izaakm@users.noreply.github.com>
2022-06-29 16:09:08 -0400

    Move main into fxn; add destination attribute to ImageGui

--- sort_folder.py
+++ sort_folder.py
@@ -17,8 +17,14 @@ import argparse
 # import Tkinter as tk
 import tkinter as tk
 import os
-from shutil import copyfile, move
+import glob
+import sys
+import shutil
+
+# from shutil import copyfile, move
 from PIL import ImageTk, Image
+
+IMAGE_EXTENSIONS = set(['.jpg', '.jpeg', '.gif', '.tif', '.tiff', '.psd', '.bmp'])
 
 class ImageGui:
     """
@@ -26,7 +32,7 @@ class ImageGui:
     Useful, for sorting views into sub views or for removing outliers from the data.
     """
 
-    def __init__(self, master, labels, paths):
+    def __init__(self, master, labels, paths, destination):
         """
         Initialise GUI
         :param master: The parent window
@@ -46,8 +52,9 @@ class ImageGui:
 
         # Start at the first file name
         self.index = 0
-        self.paths = paths
         self.labels = labels
+        self.paths = paths
+        self.destination = destination
 
         # Number of labels and paths
         self.n_labels = len(labels)
@@ -116,7 +123,7 @@ class ImageGui:
         :param label: The label that the user voted for
         """
         input_path = self.paths[self.index]
-        self._copy_image(input_path, label)
+        self._copy_image(input_path, self.destination, label)
         self.show_next_image()
 
     def vote_key(self, event):
@@ -137,24 +144,34 @@ class ImageGui:
         :return: Resized image
         """
         image = Image.open(path)
-        image = image.resize(size, Image.ANTIALIAS)
+        # image = image.resize(size, Image.ANTIALIAS)
+        # From image-sorter2:
+        max_height = 500
+        img = image 
+        s = img.size
+        ratio = max_height / s[1]
+        image = img.resize((int(s[0]*ratio), int(s[1]*ratio)), Image.ANTIALIAS)
         return image
 
     @staticmethod
-    def _copy_image(input_path, label):
+    def _copy_image(input_path, destination, label):
         """
         Copies a file to a new label folder using the shutil library. The file will be copied into a
         subdirectory called label in the input folder.
         :param input_path: Path of the original image
         :param label: The label
         """
-        root, file_name = os.path.split(input_path)
-        output_path = os.path.join(root, label, file_name)
-        print(" %s --> %s" % (file_name, label))
-        copyfile(input_path, output_path)
+        _, file_name = os.path.split(input_path)
+        # output_path = os.path.join(dirname, label, file_name)
+        output_folder = os.path.join(destination, label)
+        output_path = os.path.join(output_folder, file_name)
+        os.makedirs(output_folder, exist_ok=True)
+        # print(" %s --> %s" % (file_name, label))
+        print(" %s -> %s" % (input_path, output_path))
+        shutil.copyfile(input_path, output_path)
 
     @staticmethod
-    def _move_image(input_path, label):
+    def _move_image(input_path, destination, label):
         """
         Moves a file to a new label folder using the shutil library. The file will be moved into a
         subdirectory called label in the input folder. This is an alternative to _copy_image, which is not
@@ -162,46 +179,88 @@ class ImageGui:
         :param input_path: Path of the original image
         :param label: The label
         """
-        root, file_name = os.path.split(input_path)
-        output_path = os.path.join(root, label, file_name)
-        print(" %s --> %s" % (file_name, label))
-        move(input_path, output_path)
+        _, file_name = os.path.split(input_path)
+        # output_path = os.path.join(dirname, label, file_name)
+        output_folder = os.path.join(destination, label)
+        output_path = os.path.join(output_folder, file_name)
+        os.makedirs(output_folder, exist_ok=True)
+        # print(" %s --> %s" % (file_name, label))
+        print(" %s -> %s" % (input_path, output_path))
+        shutil.move(input_path, output_path)
 
 
-def make_folder(directory):
-    """
-    Make folder if it doesn't already exist
-    :param directory: The folder destination path
-    """
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+# def make_folder(directory):
+#     """
+#     Make folder if it doesn't already exist
+#     :param directory: The folder destination path
+#     """
+#     if not os.path.exists(directory):
+#         os.makedirs(directory)
 
-# The main bit of the script only gets exectured if it is directly called
-if __name__ == "__main__":
+
+def find_images(dirname):
+    '''
+    Put all image file paths into a list
+    '''
+    paths = []
+    for path in glob.glob(os.path.join(dirname, '**'), recursive=True):
+        _, ext = os.path.splitext(path)
+        if os.path.isdir(path):
+            continue
+        elif ext.lower() in IMAGE_EXTENSIONS:
+            paths.append(path)
+        else:
+            pass
+    return paths
+
+
+def main():
 
     # Make input arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--folder', help='Input folder where the *tif images should be', required=True)
+
     parser.add_argument('-l', '--labels', nargs='+', help='Possible labels in the images', required=True)
+    parser.add_argument('-o', '--output-folder', help='Copy images to <output-folder>/<label>', required=False, default='sorted')
+
+    # [TODO] Mutually exclusive group:
+    parser.add_argument('--images', nargs='*', required=False)
+    parser.add_argument('-f', '--input-folder', help='Find images in this directory.', required=False)
+
     args = parser.parse_args()
+    print(args)
 
     # grab input arguments from args structure
-    input_folder = args.folder
+    # input_folder = args.folder
     labels = args.labels
+    output_folder = args.output_folder
 
-    # Make folder for the new labels
-    for label in labels:
-        make_folder(os.path.join(input_folder, label))
+    # # Make folder for the new labels
+    # for label in labels:
+    #     # make_folder(os.path.join(output_folder, label))
+    #     dirname = os.path.join(output_folder, label)
+    #     os.makedirs(dirname, exist_ok=True)
 
-    # Put all image file paths into a list
-    paths = []
-    for file in os.listdir(input_folder):
-        if file.endswith(".tif") or file.endswith(".tiff"):
+    if isinstance(args.images, list):
+        paths = args.images
+    else:
+        # Put all image file paths into a list
+        paths = find_images(args.input_folder)
+    # print(*paths, sep='\n')
 
-            path = os.path.join(input_folder, file)
-            paths.append(path)
+    if not paths:
+        print('[ERROR] No images found.')
+        sys.exit(1)
 
     # Start the GUI
-    root = tk.Tk()
-    app = ImageGui(root, labels, paths)
-    root.mainloop()
+    master = tk.Tk()
+    app = ImageGui(master, labels, paths, output_folder)
+    master.mainloop()
+
+    return 0
+
+
+if __name__ == "__main__":
+    res = main()
+    sys.exit(res)
+
+# END.
